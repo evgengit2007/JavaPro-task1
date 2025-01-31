7acf83cc0bbd479f7cbc63d85e69cd0cd9b416b9
evgengit2007 <166688467+evgengit2007@users.noreply.github.com>
2025-01-31 13:17:43 +0700

    Переделан runMethod, работа с параметрами

--- src/main/java/ru/vtb/javaPro/Runner.java
+++ src/main/java/ru/vtb/javaPro/Runner.java
@@ -52,13 +52,13 @@ public class Runner {
         System.out.println("    runMethodTest3 started");
     }
 
-    @CsvSource("10, Java, 20, true, 2.5, 2025")
+    @CsvSource("10,Java,20,true,2.5,2025")
     public void csvMethod(int a, String b, int c, boolean d, double e, long l) {
         System.out.println("    csvMethod started: a = " + a +
                 "; b = " + b + "; c = " + c + "; d = " + d +
                 "; e = " + e + "; l = " + l);
     }
-    @CsvSource("10, true, 20, Java, 2.5, 2025")
+    @CsvSource("10,true,20,Java,2.5,2025")
     public void csvMethodDouble(int a, boolean b, int c, String s, double d, long l) {
         System.out.println("    csvMethodDouble started: a = " + a +
                 "; b = " + b + "; s = " + s + "; d = " + d +
